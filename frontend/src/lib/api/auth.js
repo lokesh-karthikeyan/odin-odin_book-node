5d5270954586282009f1ae0de2fd22f3c8db36bb
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2026-02-26 23:11:04 +0530

    Feat: Add req/res helpers for auth endpoints

---
+++ frontend/src/lib/api/auth.js
@@ -0,0 +1,26 @@
+const BASE_URL = 'http://localhost:3000/api/v1/auth';
+
+async function request(endpoint, data) {
+  const reqData = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+    credentials: "include"
+  };
+
+  const res = await fetch(`${BASE_URL}/${endpoint}`, reqData);
+  return res.json();
+}
+
+async function login({ email, password }) {
+  return request("login", { email, password });
+}
+
+async function register({ fullName, email, password }) {
+  return request("register", { fullName, email, password });
+}
+
+export {
+  login,
+  register,
+}
