efb1d128657d38ec12cc4b72444e5d6764e2da22
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2026-02-28 20:53:40 +0530

    Style: Fix padding on mobile view

--- frontend/src/routes/(app)/+layout.svelte
+++ frontend/src/routes/(app)/+layout.svelte
@@ -161,5 +161,9 @@
     overflow-y: auto;
     background: color-mix(in srgb, var(--bg-primary), black 3%);
     box-shadow: rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px;
+
+    @media (max-width: 768px) {
+      padding: 1em;
+    }
   }
 </style>
