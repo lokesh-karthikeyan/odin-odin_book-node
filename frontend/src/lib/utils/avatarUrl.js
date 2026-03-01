738767ea21ef4122446e4165007f166901908b26
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2026-03-01 17:25:06 +0530

    Feat: Add helper to show profile picture url

---
+++ frontend/src/lib/utils/avatarUrl.js
@@ -0,0 +1,14 @@
+import { PUBLIC_API_URL } from '$env/static/public';
+
+export function getAvatarUrl({ picture, fullName } = {}) {
+  console.log(picture)
+  console.log(fullName)
+  if (picture) {
+    if (picture.startsWith('http')) return picture;
+    return `${PUBLIC_API_URL}${picture}`;
+  }
+
+  return `https://ui-avatars.com/api/?name=${encodeURIComponent(
+    fullName || 'User'
+  )}&background=random&color=random`;
+}
