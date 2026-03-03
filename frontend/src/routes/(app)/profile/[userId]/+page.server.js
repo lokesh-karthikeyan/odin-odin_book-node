ee4fbeb4d95092b5afe13824e4b8f4d2bcc783ad
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2026-03-03 20:49:26 +0530

    Feat: Add newsfeed on user's profile page

--- frontend/src/routes/(app)/profile/[userId]/+page.server.js
+++ frontend/src/routes/(app)/profile/[userId]/+page.server.js
@@ -1,13 +1,19 @@
-import { profileApi } from '$lib/api';
+import { postsApi, profileApi } from '$lib/api';
 import { error } from '@sveltejs/kit';
 
 export async function load({ params, fetch, parent }) {
   const { token } = await parent();
   const { userId } = params;
 
-  const res = await profileApi.getProfile(fetch, token, userId);
+  const [profileRes, postsRes] = await Promise.all([
+    profileApi.getProfile(fetch, token, userId),
+    postsApi.getFeed(fetch, token, userId),
+  ])
 
-  if (!res.success) throw error(404, "User not found");
+  if (!profileRes.success) throw error(404, "User not found");
 
-  return { profile: res.data.profile };
+  return {
+    profile: profileRes.data.profile,
+    posts: postsRes.data.posts || [],
+  };
 }
